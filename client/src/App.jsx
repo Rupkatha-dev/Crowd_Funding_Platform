d86a736972d367932d94de800c03a3d06a005db9
Rupkatha-dev <roy.rupkatha@yahoo.com>
2025-02-22 12:23:43 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div><h1>Hi welcome to my first react app, my project is crowd funding platform</h1></div>
+        
+  )
+}
+
+export default App
---
+++ client/src/components/About.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function About()
+{
+    return(
+        <div><h2>About Page</h2></div>
+    )
+}
+export default About
